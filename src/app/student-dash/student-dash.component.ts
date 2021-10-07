7774362cb5e3e9d2ee10ce2a0bf4feeeeb50edbc
Prajwal Kadam <prajwalkadam03@gmail.com>
2021-10-07 18:02:44 +0530

    Project Completed

--- src/app/student-dash/student-dash.component.ts
+++ src/app/student-dash/student-dash.component.ts
@@ -9,9 +9,13 @@ import { APIService } from '../Shared/api.service';
   styleUrls: ['./student-dash.component.css']
 })
 export class StudentDashComponent implements OnInit {
-  formValue !:any;
+  ShowAdd !: boolean;
+  ShowUpdate !: boolean;
 
+  formValue !:any;
   StudentModelsobj:StudentDashModel = new StudentDashModel();
+
+  studentAll:any;
 
   constructor(private formBuilder:FormBuilder,private api : APIService) {
 
@@ -26,6 +30,15 @@ export class StudentDashComponent implements OnInit {
       Fees:[],
       Address:[],
     })
+    this.getAllStudents()
+
+  }
+
+  ClickAddStudent()
+  {
+    this.formValue.reset();
+    this.ShowAdd=true;
+    this.ShowUpdate=false;
   }
    PostStudentDetails()
    {
@@ -33,14 +46,17 @@ export class StudentDashComponent implements OnInit {
      this.StudentModelsobj.lastName = this.formValue.value.lastName;
      this.StudentModelsobj.email = this.formValue.value.Email;
      this.StudentModelsobj.mobile = this.formValue.value.Mobile;
-     this.StudentModelsobj.fees = this.formValue.value.fees;
-     this.StudentModelsobj.address = this.formValue.value.Address;
+     this.StudentModelsobj.fees = this.formValue.value.Fees;
+    // this.StudentModelsobj.address = this.formValue.value.Address;
 
+  
 
 this.api.postStudent(this.StudentModelsobj).subscribe(res=>{
   console.log(res);
   alert("Student record added successfully");
   this.formValue.reset();
+  this.getAllStudents();
+
 },
 err=>{
   alert("somthing went wong !!!!" );
@@ -48,4 +64,45 @@ err=>{
 
    }
 
+   getAllStudents()
+   {
+     this.api.getStudent().subscribe(res=>{
+       this.studentAll = res;
+     })
+   }
+
+   deleteStudent(data:any)
+   {
+     this.api.deleteStudent(data.id).subscribe(res=>{
+       alert("records Deleted Sucessfully");
+       this.getAllStudents();
+     })
+   }
+
+   onEdit(data:any)
+   {
+    this.ShowAdd=false;
+    this.ShowUpdate=true;
+     this.StudentModelsobj.ID=data.id;
+     this.formValue.controls['firstName'].setValue(data.firstName);
+     this.formValue.controls['lastName'].setValue(data.lastName);
+     this.formValue.controls['Email'].setValue(data.email);
+     this.formValue.controls['Mobile'].setValue(data.mobile);
+     this.formValue.controls['Fees'].setValue(data.fees);
+   }
+   updateStudentDetails()
+     {
+      this.StudentModelsobj.firstName = this.formValue.value.firstName;
+      this.StudentModelsobj.lastName = this.formValue.value.lastName;
+     this.StudentModelsobj.email = this.formValue.value.Email;
+     this.StudentModelsobj.mobile = this.formValue.value.Mobile;
+     this.StudentModelsobj.fees = this.formValue.value.Fees;
+
+      this.api.updateStudent(this.StudentModelsobj,this.StudentModelsobj.ID).subscribe(res=>{
+        alert("Record added successfully..");
+        this.formValue.reset();
+        this.getAllStudents();
+      })
+     }
+     
 }
